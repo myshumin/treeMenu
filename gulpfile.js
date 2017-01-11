0ab01b8675246550492d313d21d06f18c89ebe9c
mrsum <shumin.1989@qq.com>
2017-01-11 20:06:32 +0800

    treeMenu
    Signed-off-by: mrsum <shumin.1989@qq.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const base64 = require('gulp-base64');
+const uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
 	return gulp.src('./src/scss/**/*.scss')
-			.pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 			.pipe(gulp.dest('./dist/css'));
 });
 
@@ -14,8 +15,14 @@ gulp.task('base64', ['sass'], function () {
 	return gulp.src('./dist/css/*.css')
 			.pipe(base64({
 				extensions: ['png'],
-				maxImageSize: 20*1024 // bytes
+				maxImageSize: 20 * 1024 // bytes
 			}))
 			.pipe(gulp.dest('./dist/css'));
 });
-gulp.task('default',['sass', 'base64']);
+
+gulp.task('compress', function () {
+	return gulp.src('./dist/js/*.js')
+			.pipe(uglify())
+			.pipe(gulp.dest('./dist/js/'));
+});
+gulp.task('default', ['sass', 'base64', 'compress']);
